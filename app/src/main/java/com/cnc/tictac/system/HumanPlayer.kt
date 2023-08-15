3db3d0767dc866226edb650fbae6e1a40b2f8ea8
ryan-n-may <nryanmay@gmail.com>
2023-08-15 12:54:32 +0800

    Added backend system classes, Classes empty.

--- app/src/main/java/com/cnc/tictac/system/HumanPlayer.kt
+++ app/src/main/java/com/cnc/tictac/system/HumanPlayer.kt
@@ -1,14 +1,18 @@
 package com.cnc.tictac.system
 
 import android.graphics.drawable.Drawable
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import com.cnc.tictac.R
+
 class HumanPlayer(
     playerName      : String                    = "Player",
                     // Player-set username.
     playerID        : Long                      = 0L,
                     // Unique player ID (system generated).
-    playerIcon      : Pair<Drawable, String>    = Pair(res.drawable.avatar, "Null Player Icon"),
+    playerIcon      : Pair<Int, String>    = Pair(R.drawable.avatar, "Null Player Icon"),
                     // Pair for player icon: Resource, and description.
-    playerAvatar    : Pair<Drawable, String>    = Pair(res.drawable.avatar, "Null Player Icon"),
+    playerAvatar    : Pair<Int, String>    = Pair(R.drawable.avatar, "Null Player Icon"),
                     // Pair for avatar image: Resource, and description.
 ) : Player (playerName, playerID, playerIcon, playerAvatar) {
 
--- app/src/main/java/com/cnc/tictac/system/Player.kt
+++ app/src/main/java/com/cnc/tictac/system/Player.kt
@@ -5,7 +5,7 @@ import android.graphics.drawable.Drawable
 abstract class Player(
     playerName  : String, // Player-set username.
     playerID    : Long, // Unique player ID (system generated).
-    playerIcon  : Pair<Drawable, String>, // Pair for player icon: Resource, and description.
-    avatar      : Pair<Drawable, String>, // Pair for avatar image: Resource, and description.
+    playerIcon  : Pair<Int, String>, // Pair for player icon: Resource, and description.
+    avatar      : Pair<Int, String>, // Pair for avatar image: Resource, and description.
 ) {
 }
