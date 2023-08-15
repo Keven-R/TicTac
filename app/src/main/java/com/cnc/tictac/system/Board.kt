54d413531c9366b631190d0d8c5d18a4b3dad947
ryan-n-may <nryanmay@gmail.com>
2023-08-15 13:16:35 +0800

    Added backend system classes, Classes empty.

--- app/src/main/java/com/cnc/tictac/system/Board.kt
+++ app/src/main/java/com/cnc/tictac/system/Board.kt
@@ -1,4 +1,27 @@
 package com.cnc.tictac.system
+import java.util.Stack
 
-class Board {
+// Board state is a nullable 2D array of players. Initially a null array.
+typealias BoardState = Array<Array<Player?>>
+class Board(
+    var boardState      : BoardState,
+    var boardHistory    : Stack<BoardState>,
+    private val width           : Int,
+    private val height          : Int,
+    private val minimumWin      : Int,
+){
+    init{
+        // Create board state of 2D array of Players (a nullable type).
+        boardState = Array(width, { Array(height, { null }) })
+        // Append initial blank board state to boardHistory stack
+        boardHistory.add(boardState)
+    }
+    /** placePuck ( currentPlayer , x, y )
+     *  This method places the player object reference in the specified x and y coordinates of the board.
+     *  Understanding which player has the turn, and other such game logic is controlled by the
+     *  GameController class.
+     */
+    fun placePuck(currentPlayer : Player, x : Int, y : Int){
+
+    }
 }
--- app/src/main/java/com/cnc/tictac/system/HumanPlayer.kt
+++ app/src/main/java/com/cnc/tictac/system/HumanPlayer.kt
@@ -7,13 +7,13 @@ import com.cnc.tictac.R
 
 class HumanPlayer(
     playerName      : String                    = "Player",
-                    // Player-set username.
+    // Player-set username.
     playerID        : Long                      = 0L,
-                    // Unique player ID (system generated).
+    // Unique player ID (system generated).
     playerIcon      : Pair<Int, String>    = Pair(R.drawable.avatar, "Null Player Icon"),
-                    // Pair for player icon: Resource, and description.
+    // Pair for player icon: Resource, and description.
     playerAvatar    : Pair<Int, String>    = Pair(R.drawable.avatar, "Null Player Icon"),
-                    // Pair for avatar image: Resource, and description.
+    // Pair for avatar image: Resource, and description.
 ) : Player (playerName, playerID, playerIcon, playerAvatar) {
 
 }
