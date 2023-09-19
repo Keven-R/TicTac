d141dc9eb7ed31a9955d63cd8d7e87ca554797b3
Jasmine Bañares <jban0004@student.monash.edu>
2023-09-19 18:45:47 +1100

    move commit to this branch

--- app/src/main/java/com/cnc/tictac/ui/screens/Leaderboard.kt
+++ app/src/main/java/com/cnc/tictac/ui/screens/Leaderboard.kt
@@ -10,7 +10,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+<<<<<<< HEAD
 import androidx.compose.material3.MaterialTheme
+=======
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+>>>>>>> c93219b (leaderboard done, needs to be ordered by num of wins)
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
