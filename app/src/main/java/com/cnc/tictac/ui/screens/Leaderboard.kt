c2133b217b9e44df311db39bef6f11e906bda2da
Jasmine Bañares <jban0004@student.monash.edu>
2023-09-19 18:46:40 +1100

    merge

--- app/src/main/java/com/cnc/tictac/ui/screens/Leaderboard.kt
+++ app/src/main/java/com/cnc/tictac/ui/screens/Leaderboard.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 <<<<<<< HEAD
+<<<<<<< HEAD
 import androidx.compose.material3.MaterialTheme
 =======
 import androidx.compose.foundation.rememberScrollState
@@ -18,6 +19,9 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 >>>>>>> c93219b (leaderboard done, needs to be ordered by num of wins)
+=======
+import androidx.compose.material3.MaterialTheme
+>>>>>>> 6379a00 (merge)
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
